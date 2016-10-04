021d0d6b0209498545a1ee61f40e265047090373
and86 <andrea.ln@tiscali.it>
2016-10-04 10:37:27 +0200

    Esercizio Rubrica

---
+++ esRubrica/src/rubrica/Voce.java
@@ -0,0 +1,49 @@
+package rubrica;
+
+public class Voce {
+	
+	private String nome;
+	private String cogname;
+	private String tel;
+	
+	public Voce() {
+		
+	}
+
+	public Voce(String nome, String cogname, String tel) {
+		this.nome = nome;
+		this.cogname = cogname;
+		this.tel = tel;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCogname() {
+		return cogname;
+	}
+
+	public void setCogname(String cogname) {
+		this.cogname = cogname;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+	
+	
+
+}
