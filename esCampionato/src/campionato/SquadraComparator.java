47e57e31b8073b752f5b6c8be87e976a585d61f1
and <andrea.ln@tiscali.it>
2016-10-08 15:33:19 +0200

    campionato: squadraComparator

--- esCampionato/src/campionato/SquadraComparator.java
+++ esCampionato/src/campionato/SquadraComparator.java
@@ -31,15 +31,26 @@ public class SquadraComparator implements Comparator <Squadra> {
 		int p1=0;
 		int p2=0;
 		
-		if(c.getMappaPartite().containsKey(s1.getNome()+" "+s2.getNome()) || (c.getMappaPartite().containsKey(s2.getNome()+" "+s1.getNome()))){
-			if(c.getMappaPartite().get(s1).getRisultato()=='1'){
+		//Partita di andata: se risultato è 1 do +3 alla squadra di casa(s1), se risultato è 2 do +3 a squadra ospite(s2)
+		if(c.getMappaPartite().containsKey(s1.getNome()+" "+s2.getNome())){
+			if(c.getMappaPartite().get(s1.getNome()+" "+s2.getNome()).getRisultato()=='1'){
 				p1=3;
-			} else if (c.getMappaPartite().get(s1).getRisultato()=='2'){
+			} else if (c.getMappaPartite().get(s1.getNome()+" "+s2.getNome()).getRisultato()=='2'){
 				p2=3;
 			}
-			
-							
+												
 		}
+		
+		//Partita di ritorno: se risultato è 1 do +3 alla squadra di casa (s2), se risultato è 2 do +3 a squadra ospite(s1) 
+		if(c.getMappaPartite().containsKey(s2.getNome()+" "+s1.getNome())){
+			if(c.getMappaPartite().get(s2.getNome()+" "+s1.getNome()).getRisultato()=='1'){
+				p2+=3;
+			} else if (c.getMappaPartite().get(s1.getNome()+" "+s2.getNome()).getRisultato()=='2'){
+				p1+=3;
+			}
+		}
+		
+		
 		int varScontriDiretti=p2-p1;
 		return varScontriDiretti;
 		
